70b977dd3227ae2d811040776aac5ba4df7f9938
DhruvK0 <kanetkard@gmail.com>
2022-06-17 17:47:52 -0700

    added backprop function to relu class

--- ANN/activation_relu.py
+++ ANN/activation_relu.py
@@ -3,3 +3,11 @@ import numpy as np
 class activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        # Since we need to modify the original variable,
+        # let's make a copy of the values first
+        self.dinputs = dvalues.copy()
+        
+        # Zero gradient where input values were negative
+        self.dinputs[self.inputs <= 0] = 0
