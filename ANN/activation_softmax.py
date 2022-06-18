b322f53944ab536301239f6af03df2eddcfdb393
DhruvK0 <kanetkard@gmail.com>
2022-06-18 11:40:13 -0700

    added backprop to softmax

--- ANN/activation_softmax.py
+++ ANN/activation_softmax.py
@@ -9,3 +9,21 @@ class activation_Softmax:
         probs = exp_vals/ np.sum(exp_vals, axis=1, keepdims=True)
 
         self.output = probs
+
+    def backward(self, dvalues):
+
+        #create empty uninitialized array
+        self.dinputs = np.empty_like(dvalues)
+
+        #enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in \
+                enumerate(zip(self.output, dvalues)):
+            #Flatten output array 
+            single_output = single_output.reshape(-1,1)
+            
+            # Calculate Jacobian matrix of the output and
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
