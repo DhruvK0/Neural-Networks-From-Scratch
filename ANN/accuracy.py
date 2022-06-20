bcd59a081d207d48f9e62fced616532e8e12ffc1
DhruvK0 <kanetkard@gmail.com>
2022-06-19 21:36:47 -0700

    updated accuracy function

--- ANN/accuracy.py
+++ ANN/accuracy.py
@@ -4,8 +4,9 @@ class Accuracy():
     # Calculate accuracy from output of activation and targets
     # calculate values along first axis
     def calculate(self, output, y_true):
+        self.y_true = y_true
         self.predictions = np.argmax(output, axis=1)
-        if len(y_true.shape) == 2:
-            self.y = np.argmax(y_true, axis=1)
+        if len(self.y_true.shape) == 2:
+            self.y_true = np.argmax(self.y_true, axis=1)
         
-        return np.mean(self.predictions == self.y)
+        return np.mean(self.predictions==self.y_true)
