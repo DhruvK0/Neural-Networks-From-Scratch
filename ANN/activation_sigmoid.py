8c79e4f375500fe44f3a63f08f0a5cfba415c3fe
DhruvK0 <kanetkard@gmail.com>
2022-06-20 12:56:09 -0700

    forward sigmoid method

---
+++ ANN/activation_sigmoid.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class activation_Sigmoid():
+    #forward pass
+    def forward(self, inputs):
+        #calculate sigmoid of inputs
+        self.inputs = inputs
+        self.outputs = 1 / (1 + np.exp(-inputs))
+    
