cc748caa67409b4fc6c00dd5ec6c8e88d3d1ced8
DhruvK0 <kanetkard@gmail.com>
2022-06-20 01:43:40 -0700

    added regularization to loss class

--- ANN/loss.py
+++ ANN/loss.py
@@ -9,4 +9,30 @@ class Loss:
         data_loss = np.mean(sample_losses)
 
         return data_loss
+    
+    #regularization loss calculation
+    def regularization_loss(self, layer):
+        # default value of 0
+        regularization_loss = 0
+
+        #L1 regularization for weights
+        if layer.weight_regularizer_l1 > 0:
+            regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
+
+        #L2 regulariztion for weights
+        if layer.weight_regularizer_l2 > 0:
+            regularization_loss += layer.weight_regularizer_l2 * np.sum(layer.weights * layer.weights)
+
+        #L1 regularization for biases
+        if layer.bias_regularizer_l1 > 0:
+            regularization_loss += layer.bias_regularizer_l1 * np.sum(np.abs(layer.biases))
+
+        #L2 regulariztion for biases
+        if layer.bias_regularizer_l2 > 0:
+            regularization_loss += layer.bias_regularizer_l2 * np.sum(layer.biases * layer.biases)
+
+        
+        return regularization_loss
+
+
 
