df34cd30e12eafa0ebabdef42bb79358d9ecd33a
DhruvK0 <kanetkard@gmail.com>
2022-06-20 16:18:21 -0700

    sigmoid function setup

--- ANN/activation_sigmoid.py
+++ ANN/activation_sigmoid.py
@@ -5,7 +5,7 @@ class activation_Sigmoid():
     def forward(self, inputs):
         #calculate sigmoid of inputs
         self.inputs = inputs
-        self.outputs = 1 / (1 + np.exp(-inputs))
+        self.output = 1 / (1 + np.exp(-inputs))
     
     def backward(self, dvalues):
-        self.dinputs = dvalues * (1 - self.outputs) * self.outputs
+        self.dinputs = dvalues * (1 - self.output) * self.output
