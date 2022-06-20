011571ca120fd9ce12893a128d0d995d2f8a3d95
DhruvK0 <kanetkard@gmail.com>
2022-06-19 21:37:06 -0700

    fixed activation function classes

--- ANN/activation_softmax.py
+++ ANN/activation_softmax.py
@@ -1,9 +1,10 @@
-from this import d
 import numpy as np
 
 class activation_Softmax:
     
     def forward(self, inputs):
+        self.inputs = inputs 
+
         exp_vals = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
 
         probs = exp_vals/ np.sum(exp_vals, axis=1, keepdims=True)
